ae20056e04524cc3b6049b351915568f6438a794
janvins85 <jan.vins@grada.cz>
2026-05-13 20:16:17 +0200

    SEO: Add JSON-LD structured data (Schema.org) to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
 import { FlowLens } from "@/components/FlowLens";
 
+const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    name: "Firemni rentgen",
+    description:
+          "Firemni rentgen pomaha malym a strednim firmam odhalit, kde ztraci cas, penize a kapacitu lidi.",
+    url: "https://www.flowlens.cz",
+    logo: "https://www.flowlens.cz/og-image.png",
+    serviceType: [
+          "Procesni analyza",
+          "Datova analytika",
+          "Business intelligence",
+          "Firemni poradenstvi",
+        ],
+    areaServed: {
+          "@type": "Country",
+          name: "Czech Republic",
+    },
+    offers: {
+          "@type": "Offer",
+          name: "Firemni rentgen",
+          description:
+                  "Prehled nad firmou behem 10-14 dni. Zjistite, kde ztraci cas, penize a kapacitu lidi.",
+    },
+};
+
 export default function Page() {
-  return <FlowLens />;
-}
+    return (
+          <>
+                <script
+                          type="application/ld+json"
+                          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+                        />
+                <FlowLens />
+          </>>
+        );
+}</>
